9351ef0dbe78124d089818cb64f2a1350516f935
Ibuki-Bobby-Inomata <ibuki.bobby@gmail.com>
2022-02-01 20:09:33 +0900

    はじめてのSocket.io#2 serve.jsの更新

--- serve.js
+++ serve.js
@@ -1,21 +1,42 @@
-const app = require("express")();
+const crypto = require('crypto');
+const app  = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io   = require("socket.io")(http);
+
+// HTMLやJSなどを配置するディレクトリ
+const DOCUMENT_ROOT = __dirname + "/public";
 
 /**
  * "/"にアクセスがあったらindex.htmlを返却
  */
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.get("/", (req, res)=>{
+    res.sendFile(DOCUMENT_ROOT + "/index.html");
+});
+app.get("/:file", (req, res)=>{
+    res.sendFile(DOCUMENT_ROOT + "/" + req.params.file);
 });
 
 /**
  * [イベント] ユーザーが接続
  */
-io.on("connection", (socket) => {
+io.on("connection", (socket)=>{
     console.log("ユーザーが接続しました");
 
-    socket.on("post", (msg) => {
+    //---------------------------------
+    // ログイン
+    //---------------------------------
+    (()=>{
+        // トークンを作成
+        const token = makeToken(socket.id);
+
+        // 本人にトークンを送付
+        io.to(socket.id).emit("token", {token:token});
+    })();
+
+    //---------------------------------
+    // 発言を全員に送信
+    //---------------------------------
+    socket.on("post", (msg)=>{
         io.emit("member-post", msg);
     });
 });
@@ -23,6 +44,17 @@ io.on("connection", (socket) => {
 /**
  * 3000番でサーバを起動する
  */
-http.listen(3000, () => {
+http.listen(3000, ()=>{
     console.log("listening on *:3000");
 });
+
+/**
+ * トークンを作成する
+ *
+ * @param  {string} id - socket.id
+ * @return {string}
+ */
+function makeToken(id){
+    const str = "aqwsedrftgyhujiko" + id;
+    return( crypto.createHash("sha1").update(str).digest('hex') );
+}

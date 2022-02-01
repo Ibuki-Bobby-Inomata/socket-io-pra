70b96325e24901d840bf92f3916c1247e4298aed
Ibuki-Bobby-Inomata <ibuki.bobby@gmail.com>
2022-02-01 20:37:36 +0900

    util.jsの名前修正

--- serve.js
+++ serve.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
-const app  = require("express")();
+const app = require("express")();
 const http = require("http").createServer(app);
-const io   = require("socket.io")(http);
+const io = require("socket.io")(http);
 
 // HTMLやJSなどを配置するディレクトリ
 const DOCUMENT_ROOT = __dirname + "/public";
@@ -9,34 +9,36 @@ const DOCUMENT_ROOT = __dirname + "/public";
 /**
  * "/"にアクセスがあったらindex.htmlを返却
  */
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.sendFile(DOCUMENT_ROOT + "/index.html");
 });
-app.get("/:file", (req, res)=>{
+app.get("/:file", (req, res) => {
     res.sendFile(DOCUMENT_ROOT + "/" + req.params.file);
 });
 
 /**
  * [イベント] ユーザーが接続
  */
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
     console.log("ユーザーが接続しました");
 
     //---------------------------------
     // ログイン
     //---------------------------------
-    (()=>{
+    (() => {
         // トークンを作成
         const token = makeToken(socket.id);
 
         // 本人にトークンを送付
-        io.to(socket.id).emit("token", {token:token});
+        io.to(socket.id).emit("token", {token: token});
+        console.log(socket);
+        console.log(token)
     })();
 
     //---------------------------------
     // 発言を全員に送信
     //---------------------------------
-    socket.on("post", (msg)=>{
+    socket.on("post", (msg) => {
         io.emit("member-post", msg);
     });
 });
@@ -44,7 +46,7 @@ io.on("connection", (socket)=>{
 /**
  * 3000番でサーバを起動する
  */
-http.listen(3000, ()=>{
+http.listen(3000, () => {
     console.log("listening on *:3000");
 });
 
@@ -54,7 +56,7 @@ http.listen(3000, ()=>{
  * @param  {string} id - socket.id
  * @return {string}
  */
-function makeToken(id){
+function makeToken(id) {
     const str = "aqwsedrftgyhujiko" + id;
-    return( crypto.createHash("sha1").update(str).digest('hex') );
+    return (crypto.createHash("sha1").update(str).digest('hex'));
 }
